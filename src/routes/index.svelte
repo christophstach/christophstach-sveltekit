255a15e5407627122a4d15d4976da5c66a792d09
Christoph Stach <christoph.stach@gmail.com>
2022-08-10 13:34:08 +0200

    Add lorem ipsum for index.html

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,32 @@
 	<title>Christoph Stach - Home</title>
 </svelte:head>
 
-<div>
-	Lalala
+<div class="prose prose-sm">
+	<h1>Age nunc isti doceant, vel tu potius quis enim ista melius?</h1>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <i>Istam voluptatem, inquit, Epicurus ignorat?</i> Quasi ego id curem, quid ille aiat aut neget. Memini me adesse P. <a href='http://loripsum.net/' target='_blank'>Duo Reges: constructio interrete.</a> </p>
+	
+	<p>Qualem igitur hominem natura inchoavit? An tu me de L. <i>Sed haec in pueris;</i> Vide, quaeso, rectumne sit. Sint ista Graecorum; <b>Ea possunt paria non esse.</b> </p>
+	
+	<ul>
+		<li>An vero displicuit ea, quae tributa est animi virtutibus tanta praestantia?</li>
+		<li>Est autem officium, quod ita factum est, ut eius facti probabilis ratio reddi possit.</li>
+		<li>Ergo illi intellegunt quid Epicurus dicat, ego non intellego?</li>
+		<li>Atque haec ita iustitiae propria sunt, ut sint virtutum reliquarum communia.</li>
+		<li>Et harum quidem rerum facilis est et expedita distinctio.</li>
+	</ul>
+	
+	
+	<blockquote cite='http://loripsum.net'>
+		Quaero igitur, quo modo hae tantae commendationes a natura profectae subito a sapientia relictae sint.
+	</blockquote>
+	
+	
+	<ol>
+		<li>At vero illa, quae Peripatetici, quae Stoici dicunt, semper tibi in ore sunt in iudiciis, in senatu.</li>
+		<li>Longum est enim ad omnia respondere, quae a te dicta sunt.</li>
+		<li>Non quaero, quid dicat, sed quid convenienter possit rationi et sententiae suae dicere.</li>
+		<li>Conclusum est enim contra Cyrenaicos satis acute, nihil ad Epicurum.</li>
+		<li>Tantum dico, magis fuisse vestrum agere Epicuri diem natalem, quam illius testamento cavere ut ageretur.</li>
+	</ol>	
 </div>
