f98a5c0356e90eaceffd4ea75a207aeac1e35c81
Christoph Stach <christoph.stach@gmail.com>
2022-08-11 11:33:44 +0200

    Better layout of the introduction

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,9 @@
 </svelte:head>
 
 <div>
-	<h3 class="text-lg">Hi my name is Christoph,</h3>
+	<h2 class="mb-4 border-b-2 border-black text-2xl">Hi my name is Christoph,</h2>
 
-	<p class="prose prose-sm prose-indigo max-w-none">
+	<p class="prose prose-sm prose-indigo max-w-none mb-2">
         I started software development at the age of 13. It quickly became a passion for me and I started to code my 
         first small projects and websites. I found most interest in websites. These days they were still developed with HTML 
         frame sets and some very basic <a href="https://www.php.net/">PHP</a> scripts.
@@ -20,9 +20,9 @@
         I even published some small extensions on the TYPO3 extension gallery.
         Later on, I switched job to a more generalized <a href="https://www.php.net/">PHP</a> full-stack engineer. 
         After working multiple years, I decided to quit and go traveling for a while.
-        Then when coming back to Germany, I moved to Berlin where I started to studying at <a href="https://www.htw-berlin.de/"></a>HTW Berlin, 
+        Then when coming back to Germany, I moved to Berlin where I started to studying at <a href="https://www.htw-berlin.de/">HTW Berlin</a>, 
 		a little later I started working  as a frontend engineer in part-time.
-		 During my studies, I also found interest in developing methods of artificial intelligence with neural networks.
+		During my studies, I also found interest in developing methods of artificial intelligence with neural networks.
         Later I focused more on methods of bioinformatics, studying the creation of pipelines for next generation sequencing data.
 		I always kept up to date with developing websites. My main interest is the frontend part. 
 		I used many frameworks like <a href="https://angular.io/">Angular</a>, <a href="https://reactjs.org/">React</a>,
